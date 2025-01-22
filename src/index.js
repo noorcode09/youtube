cfa2d068ef12d4d98dcf00ab830fbb1438ac6719
noor code <noorcode09@gmail.com>
2025-01-22 14:31:24 +0530

    userSchema

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // require("dotenv").config({path: './env'})
-import dotenv from 'dotenv'
-
-
+import dotenv from 'dotenv';
+import app from './app';
 import connectDB from "./db/index.js";
 
 dotenv.config({
@@ -9,6 +8,18 @@ dotenv.config({
 })
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port: ${process.env.PORT}`);
+    });
+    app.on( (error) => {
+        console.log("ERROR : ", error)
+        throw error;
+    })
+})
+.catch( (err) => {
+    console.log(`MONGO db connection failed !!!`, err)
+})
 
 
 
